6fe94dac11be23b10e4d18bf572b549e0a965ce8
Andres Gutierrez <andresgutierrez535@gmail.com>
2022-04-08 03:21:01 -0700

    test auth0

--- src/routes/authorize.svelte
+++ src/routes/authorize.svelte
@@ -1,27 +1,43 @@
 <script lang="ts">
-	import createAuth0Client, { Auth0Client } from '@auth0/auth0-spa-js';
-	import { onMount } from 'svelte';
+	import createAuth0Client, { Auth0Client } from '@auth0/auth0-spa-js'
+	import { onMount } from 'svelte'
 
-	let client: Auth0Client;
+	let client: Auth0Client
 
-	let authUrl: string;
+	let isAuthenticated = false
+	let authUrl: string
 
 	onMount(async () => {
 		client = await createAuth0Client({
-			domain: 'auth.amamcrypto.com',
-			client_id: 'DCwROhnovvs5P5rmRl6B1asjtRdxcRtY'
-		});
+			domain: 'www.auth.amamcrypto.com',
+			client_id: 'DCwROhnovvs5P5rmRl6B1asjtRdxcRtY',
+			redirect_uri: window.location.origin,
+		})
 
-		console.log(client);
-		console.log(await client.isAuthenticated());
-	});
+		console.log(client)
+		isAuthenticated = await client.isAuthenticated()
+
+		console.log(isAuthenticated)
+	})
 
 	async function attemptLogin() {
-		authUrl = await client.buildAuthorizeUrl({
-			redirect_uri: window.location.origin
-		});
+		client.loginWithPopup()
+		// authUrl = await client.buildAuthorizeUrl({
+		// 	// redirect_uri: window.location.origin
+		// });
 
-		console.log(authUrl);
+		console.log(authUrl)
+
+		// client.
+	}
+
+	async function getToken() {
+		return await client.getTokenSilently()
+	}
+
+	async function test() {
+		const token = await getToken()
+		console.log(token)
 	}
 </script>
 
@@ -30,3 +46,16 @@
 {#if authUrl}
 	<iframe src={authUrl} />
 {/if}
+
+{#if isAuthenticated}
+	<h1>Yo're sgned in!</h1>
+
+	<button on:click={test}>SUBMIT A THING</button>
+{/if}
+
+<style lang="scss">
+	iframe {
+		border: none;
+		width: 100%;
+	}
+</style>
